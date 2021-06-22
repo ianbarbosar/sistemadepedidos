1ccc187662783315a5f43eb15221a7b9c4f91789
Ian Barbosa <ianbarbosar@gmail.com>
2021-06-22 11:05:08 -0300

    Projeto criado

---
+++ src/test/java/com/ianbarbosa/sistemadepedidos/SistemadepedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianbarbosa.sistemadepedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadepedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
