2e925ce91d1f6f1747d6dd7723a102cd4160a6d1
Ian Barbosa <ianbarbosar@gmail.com>
2021-06-22 15:42:12 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/ianbarbosa/sistemadepedidos/domain/Categoria.java
+++ src/main/java/com/ianbarbosa/sistemadepedidos/domain/Categoria.java
@@ -4,13 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
