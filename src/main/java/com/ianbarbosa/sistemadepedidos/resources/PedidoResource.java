3556546069ff31961900554f26c7f9b7de600cac
Ian Barbosa <ianbarbosar@gmail.com>
2021-06-25 11:43:44 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/ianbarbosa/sistemadepedidos/resources/PedidoResource.java
+++ src/main/java/com/ianbarbosa/sistemadepedidos/resources/PedidoResource.java
@@ -17,7 +17,7 @@ public class PedidoResource {
     PedidoService pedidoService;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> find(@PathVariable Integer id) {
+    public ResponseEntity<Pedido> find(@PathVariable Integer id) {
         Pedido obj = pedidoService.find(id);
         return ResponseEntity.ok().body(obj);
     }
--- src/main/java/com/ianbarbosa/sistemadepedidos/services/CategoriaService.java
+++ src/main/java/com/ianbarbosa/sistemadepedidos/services/CategoriaService.java
@@ -19,9 +19,13 @@ public class CategoriaService {
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
-    public Categoria insert(Categoria obj){
+    public Categoria insert(Categoria obj) {
         obj.setId(null);
         return categoriaRepository.save(obj);
     }
 
+    public Categoria update(Categoria obj) {
+        find(obj.getId());
+        return categoriaRepository.save(obj);
+    }
 }
